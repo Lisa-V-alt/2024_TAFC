45c6e1b3abf7a15266498e01dacbd542c70a65f0
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-24 11:21:49 +1000

    Added Challenges and speeds

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Chelonoidis niger" targetTime={1}/>
+        <TimerChallenge title="Oryctolagus cuniculus" targetTime={5}/>
+        <TimerChallenge title="Acinonyx jubatus" targetTime={10}/>
+        <TimerChallenge title="Falco peregrinus" targetTime={15}/>
+      </div>
     </>
   );
 }
