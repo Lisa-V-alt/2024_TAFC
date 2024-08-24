6cde264bb117508a05506e94fa84726b26908bee
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-24 13:46:06 +1000

    wrapped modal under portal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef (function ResultModal({ targetTime, remainingTime, onReset }, ref){
     const dialog = useRef();
@@ -15,14 +16,15 @@ const ResultModal = forwardRef (function ResultModal({ targetTime, remainingTime
         };
     });
 
-    return(
+    return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You lost </h2>}
         {!userLost && <h2>Your Score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
         <form method="dialog" onSubmit={onReset}><button>Close</button></form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
     );
 })
 
