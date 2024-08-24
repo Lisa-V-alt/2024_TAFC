c4b560a324da8a3e027230d3412f5a88e5df514d
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-24 13:01:58 +1000

    now using interval instead of timeout

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,25 +1,34 @@
 import { useState, useRef } from 'react';
+import ResultModal from './ResultModal.jsx';
 
 export default function TimerChallenge({title, targetTime}){
     const timer = useRef();
+    const dialog = useRef();
 
-    const[timerStarted, setTimerStarted] = useState(false);
-    const[timerExpired, setTimerExpired]=useState(false);
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if (timeRemaining <= 0){
+        clearInterval(timer.current);
+        setTimeRemaining(targetTime * 1000);
+    }
 
     function handleStart(){
-        timer.current = setTimeout(() => {setTimerExpired(true);}, targetTime * 1000);
-        
-        setTimerStarted(true);
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
     }
 
     function handleStop(){
-        clearTimeout(timer.current);
+        clearInterval(timer.current);
     }
 
     return(
+        <>
+        <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
         <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
@@ -32,5 +41,6 @@ export default function TimerChallenge({title, targetTime}){
             {timerStarted ? 'Time is running ...' : 'Timer inactive'}
         </p>
         </section>
+        </>
     )
 }
